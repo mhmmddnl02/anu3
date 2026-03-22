24195c7bb22aaf7fdb29a54c3d83f62885ab23f4
mhmmddnl02 <danil.md02@gmail.com>
2026-03-22 10:06:12 +0700

    Update engsel.py

--- app/client/engsel.py
+++ app/client/engsel.py
@@ -81,7 +81,9 @@ def get_profile(api_key: str, access_token: str, id_token: str) -> dict:
     print("Fetching profile...")
     res = send_api_request(api_key, path, raw_payload, id_token, "POST")
 
-    return res.get("data")
+print("RAW RESPONSE:", res)   # <-- TAMBAH INI
+
+return res.get("data")
 
 def get_balance(api_key: str, id_token: str) -> dict:
     path = "api/v8/packages/balance-and-credit"
@@ -515,3 +517,4 @@ def dashboard_segments(
     res = send_api_request(api_key, path, raw_payload, tokens["id_token"], "POST")
 
     return res
+    
